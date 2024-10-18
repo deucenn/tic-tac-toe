585c31f4c7a8ce747d9c3fd7f222f79278369abc
deucenn <panix.black@web.de>
2024-10-18 12:00:11 +0200

    Setting the winning conditions

--- script.js
+++ script.js
@@ -1,23 +1,76 @@
 // Set Variables
-const gameField = document.querySelector(".field");
+const gameField = document.querySelectorAll(".field");
 const turnMessage = document.querySelector(".message");
 const resultMessage = document.querySelector(".result");
-const resetBtn = docuemnt.querySelector(".reset-button");
+const resetBtn = document.querySelector(".reset-button");
 
 // Initialize game board
+// const gameBoard = {
+//     cells: [],
+//     currentPlayer: "X",
+//     isGameOver: false,
+//     winningCombinations: [
+//         [0, 1, 2],
+//         [3, 4, 5],
+//         [6, 7, 8],
+//         [0, 3, 6],
+//         [1, 4, 7],
+//         [2, 5, 8],
+//         [0, 4, 8],
+//         [2, 4, 6]
+//     ]
+// };
+
 const gameBoard = {
-    cells: [],
-    currentPlayer: "X",
-    isGameOver: false,
-    winningCombinations: [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
+  board: Array(9).fill(null),
+
+  resetBoard() {
+    this.board = Array(9).fill(null);
+  },
+
+  setMarker(index, symbol) {
+    if (this.board[index] === null) {
+      this.board[index] = symbol;
+      return true;
+    }
+    return false;
+  },
+
+  winningConditions: [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ],
 };
 
+const playerX = {
+  symbol: "X",
+  picks: [],
+};
+
+const playerO = {
+  symbol: "O",
+  picks: [],
+};
+
+let currentPlayer = playerX;
+let gameOver = false;
+
+gameField.addEventListener("click", () => {
+  if (gameField.value !== "X" || gameFieldvalue !== "O") {
+    gameField.value = currentPlayer.symbol;
+    gameBoard.board.push(gameField.value);
+    currentPlayer.picks.push(parseInt(gameField.data - index));
+  }
+});
+
+const checWin(playerPicks) {
+    return gameBoard.winningCombinations.some((combination) => {
+      return combination.every((index) => playerPicks.includes(index));
+    });
+}
